44ee9667c60e08a4c718d4a1e3ddd777328b970d
TheLazyFoxJumpedOverBrownDog <billsingha1@gmail.com>
2025-09-20 18:35:57 +0000

    Save progress from Codespaces

---
+++ games.md
@@ -0,0 +1,7 @@
+    <a class="game-link bstars" href="basketball-stars/index.html">🏀 Basketball Stars 🏀</a>
+    <a class="game-link soccer" href="slsoccer/index.html">⚽ Super Liquid Soccer 🥅</a>
+    <a class="game-link bitlife" href="bitlife/bitlife.html">🧬 BitLife 🧬</a>
+    <a class="game-link swc" href="smallworldcup/index.html">⚽ A Small World Cup ⚽</a>
+    <a class="game-link ovo" href="https://script.google.com/macros/s/AKfycbyDZOJq86UIFlMkKfvZtA_Sv86sKooVRpwVFS2rb38TOT8ExCt3PfR1Y5UAveVuVGlQLw/exec">OVO</a>
+    <a class="game-link drive-mad" href="drive-mad/index.html">🚗 Drive Mad 😡</a>
+    <a class="game-link drift-boss" href="Driftboss/index.html">🚗 Drift Boss 🤵</a>
--- vite.config.js
+++ vite.config.js
@@ -7,9 +7,43 @@ export default defineConfig(async ({ command, mode }) => {
       outDir: "deploy/_site",
       rollupOptions: {
         input: {
-          main: 'index.html',
+          main: "index.html",
+          awesometanks2: "awesometanks2/index.html",
+          basketballstars: "basketball-stars/index.html",
+          bitlife: "bitlife/bitlife.html",
+          404: "404.html",
         },
-      }
-    }
-  }
+      },
+    },
+    server: {
+      headers: {
+        // Unity WebGL often needs COOP/COEP for WASM
+        "Cross-Origin-Embedder-Policy": "require-corp",
+        "Cross-Origin-Opener-Policy": "same-origin",
+      },
+    },
+    plugins: [
+      {
+        name: "unity-br-middleware",
+        configureServer(server) {
+          server.middlewares.use((req, res, next) => {
+            if (req.url.endsWith(".wasm")) {
+              req.url += ".br";
+              res.setHeader("Content-Encoding", "br");
+              res.setHeader("Content-Type", "application/wasm");
+            } else if (req.url.endsWith(".js")) {
+              req.url += ".br";
+              res.setHeader("Content-Encoding", "br");
+              res.setHeader("Content-Type", "application/javascript");
+            } else if (req.url.endsWith(".data")) {
+              req.url += ".br";
+              res.setHeader("Content-Encoding", "br");
+              res.setHeader("Content-Type", "application/octet-stream");
+            }
+            next();
+          });
+        },
+      },
+    ],
+  };
 });
